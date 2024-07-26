5f12ef651d0421565cbe469e1a80fa6b8f1ea788
asadahmad27 <amasadmughal@gmail.com>
2024-07-27 01:34:57 +0500

    function call again on polling

--- frontend/src/components/emailsData.jsx
+++ frontend/src/components/emailsData.jsx
@@ -23,6 +23,7 @@ const EmailsData = () => {
             })
           );
           setData(dataArray);
+          onSelectMail(selectedMail?.email);
         } else {
           setData([]);
         }
